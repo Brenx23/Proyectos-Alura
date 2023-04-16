4c7400d57b7bb5f533dadfb6a7c7c657b414541a
Brenx <byr101108@gmail.com>
2023-04-16 09:11:42 -0600

    Practicando funciones, Variables, concatenado y return. Todo esto para lograr hacer un programa de medicion de masa corporal con el peso y altura de la persona.

---
+++ Logica de programacion 1/.js
@@ -0,0 +1,7 @@
+var tanque = 40
+var recorridoConGasolina = 480
+console.log("¿Cual es la eficiencia del carro usando gasolina?" + (recorrido/tanque));
+
+//con alcohol
+var recorrido = 300
+console.log("¿Cual es la eficiencia del carro usando alcohol? " + (recorrido/tanque));
